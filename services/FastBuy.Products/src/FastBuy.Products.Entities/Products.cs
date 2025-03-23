d5ae2f268f3b5a8c83d3e768fa78eda6cafa826d
arielRas <ariel.ras.90@gmail.com>
2025-03-23 01:06:30 -0300

    Se crea capa Entities y se agrega la clase Products

---
+++ services/FastBuy.Products/src/FastBuy.Products.Entities/Products.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastBuy.Products.Entities;
+
+public class Products
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    public required string Description { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+    
+    [Required]
+    public DateTimeOffset CreatedAt { get; set; }
+}
