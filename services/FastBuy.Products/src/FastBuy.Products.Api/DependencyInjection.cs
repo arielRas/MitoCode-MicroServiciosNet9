96a3fd51dd70938c4f834517b68d0f235af80578
arielRas <ariel.ras.90@gmail.com>
2025-03-25 15:37:13 -0300

    Se crea la clase DependencyInyection para agregar servicios y configuraciones al Program.cs. Se agregan los serializadores y servicios de configuracion de MongoDb a la clase DependencyInyection. Se agrega al AppSettings connectionString de la BD. Se crea clase ServiceSettings en la capa Entity a fines de usar Option Pattern.

---
+++ services/FastBuy.Products/src/FastBuy.Products.Api/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using FastBuy.Products.Entities.Configuration;
+using FastBuy.Products.Repositories.Abstractions;
+using FastBuy.Products.Repositories.Implementations;
+using FastBuy.Products.Services.Abstractions;
+using FastBuy.Products.Services.Implementation;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace FastBuy.Products.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicactionServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //MongoDb Serializer
+            BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
+
+            //MongoDb Service registration
+            services.AddSingleton<IMongoClient>(serviceProvider =>
+                new MongoClient(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IMongoDatabase>(serviceProvider =>
+            {
+                var settings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>()
+                    ?? throw new ArgumentException($"The {nameof(ServiceSettings)} key has not been configured in the configuration file.");
+
+                return serviceProvider.GetRequiredService<IMongoClient>()
+                                      .GetDatabase(settings.ServiceName);
+            });
+
+            //Service registration
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+
+            return services;
+        }
+    }
+}
---
+++ services/FastBuy.Products/src/FastBuy.Products.Entities/Configuration/ServiceSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace FastBuy.Products.Entities.Configuration
+{
+    public class ServiceSettings
+    {
+        public required string ServiceName { get; set; }
+    }
+}
