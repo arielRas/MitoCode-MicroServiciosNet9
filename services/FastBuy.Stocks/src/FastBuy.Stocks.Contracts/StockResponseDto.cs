8b4c14b618d85e076a77bb10bba8d40be4ffd30c
arielRas <ariel.ras.90@gmail.com>
2025-03-25 22:48:34 -0300

    Se agrega a la solucion el proyecto FastBuy.Stocks.Contracts. Se creatn los DTOs StockDecreaseRequestDto y StockResponseDto

---
+++ services/FastBuy.Stocks/src/FastBuy.Stocks.Contracts/StockResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FastBuy.Stocks.Contracts
+{
+    public record StockResponseDto
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTimeOffset LastUpdate { get; set; }
+    }
+}
