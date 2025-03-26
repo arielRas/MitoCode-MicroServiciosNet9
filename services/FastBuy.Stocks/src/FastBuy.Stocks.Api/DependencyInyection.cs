fdd458934daf1d800127d1dd457e43e3c746772f
arielRas <ariel.ras.90@gmail.com>
2025-03-25 22:07:42 -0300

    Se crea solucion para el servicio FastBuy.Stocks. Se agrega proyecto FastBuy.Stock.Api. Se agrega liberia Swashbuckle.AspNetCore y se configura swagger como explorador de endpoints

---
+++ services/FastBuy.Stocks/src/FastBuy.Stocks.Api/DependencyInyection.cs
@@ -0,0 +1,17 @@
+﻿namespace FastBuy.Stocks.Api
+{
+    public static class DependencyInyection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            //Controller registration
+            services.AddControllers();
+
+            //configure Swagger as an endpoint explorer
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
---
+++ services/FastBuy.Stocks/src/FastBuy.Stocks.Api/Program.cs
@@ -0,0 +1,34 @@
+
+namespace FastBuy.Stocks.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddPresentation();
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
