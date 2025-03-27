47f5dbea249092281dd3cb7d4a5c8ca947abc2d8
arielRas <ariel.ras.90@gmail.com>
2025-03-27 20:00:41 -0300

    Se crea Excepcion personalizada MicroserviceComunicationException en la capa Entities. Se crea el cliente http ProductsClient y el metodo asincrono GetProductByIdAsync para obter productos del microservicio products.

---
+++ services/FastBuy.Stocks/src/FastBuy.Stocks.Entities/Exceptions/MicroserviceComunicationException.cs
@@ -0,0 +1,20 @@
+﻿namespace FastBuy.Stocks.Entities.Exceptions
+{
+    public class MicroserviceComunicationException : Exception
+    {
+        public MicroserviceComunicationException() : base ("A connection error occurred between services")
+        {
+            
+        }
+
+        public MicroserviceComunicationException(string message) : base(message)
+        {
+            
+        }
+
+        public MicroserviceComunicationException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        }
+    }
+}
