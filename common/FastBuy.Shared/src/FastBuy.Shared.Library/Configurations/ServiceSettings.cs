600cd806f5b041da35e6e64aa9efb6e1fa84cf2e
arielRas <ariel.ras.90@gmail.com>
2025-03-30 11:18:10 -0300

    Se crea la solucion FastBuy.Shared y en el se agrega el proyecto FastBuy.Shared.Library. Se instalan los paquetes MassTransit.AspNetCore, MassTransit.RabbitMq y Microsoft.Extensions.Configuration.Binder en FastBuy.Shared.Library. Se crean las clases BrokerSettings y ServiceSettings. Se agrega codigo para abstraer la configuracion del message broker en la clase MassTransitExtensions

---
+++ common/FastBuy.Shared/src/FastBuy.Shared.Library/Configurations/ServiceSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace FastBuy.Shared.Library.Configurations
+{
+    public class ServiceSettings
+    {
+        public required string ServiceName { get; set; }
+    }
+}
---
+++ common/FastBuy.Shared/src/FastBuy.Shared.Library/Messaging/MassTransitExtensions.cs
@@ -0,0 +1,40 @@
+﻿using FastBuy.Shared.Library.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MassTransit;
+
+namespace FastBuy.Shared.Library.Messaging
+{
+    public static class MassTransitExtensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Settings values 
+            var serviceSetting = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>()
+                ?? throw new ArgumentException($"The {nameof(ServiceSettings)} key has not been configured in the configuration file.");
+
+            var brokerSetting = configuration.GetSection(nameof(BrokerSettings)).Get<BrokerSettings>()
+                        ?? throw new ArgumentException($"The {nameof(BrokerSettings)} key has not been configured in the configuration file.");
+
+            //MassTransint and RabbitMq registration
+            services.AddMassTransit(configure =>
+            {
+                configure.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(brokerSetting.Host);
+
+                    configurator.ConfigureEndpoints(
+                        context,
+                        new KebabCaseEndpointNameFormatter(
+                            prefix: serviceSetting.ServiceName,
+                            includeNamespace: false
+                        )
+                    );
+
+                });
+            });
+
+            return services;
+        }
+    }
+}
