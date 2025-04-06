03b8fc37ac750d6e1641ba05737b55402d82d67a
arielRas <ariel.ras.90@gmail.com>
2025-04-06 12:12:42 -0300

    Se crean consumidores para ProductChangeEvent y ProductDeletedEvent. Estas clases actuan sobre la BBDD de Stocks

---
+++ services/FastBuy.Stocks/src/FastBuy.Stocks.Services/Consumers/ProductItemChangeConsumer.cs
@@ -0,0 +1,41 @@
+﻿using FastBuy.Products.Contracts.Events;
+using FastBuy.Shared.Library.Repository.Abstractions;
+using FastBuy.Stocks.Entities;
+using MassTransit;
+using System.Linq.Expressions;
+
+namespace FastBuy.Stocks.Services.Consumers
+{
+    public class ProductItemChangeConsumer : IConsumer<ProductChangeEvent>
+    {
+        private readonly IRepository<ProductItem> _repository;
+
+        public ProductItemChangeConsumer(IRepository<ProductItem> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Consume(ConsumeContext<ProductChangeEvent> context)
+        {
+            var message = context.Message;
+
+            Expression<Func<ProductItem, bool>> filter = x => x.Id == message.Id;
+
+            var ProductItem = new ProductItem
+            {
+                Id = message.Id,
+                Name = message.Name,
+                Description = message.Description,
+            };
+
+            if (await _repository.ExistAsync(filter))
+            {
+                await _repository.UpdateAsync(ProductItem.Id, ProductItem);
+            }
+            else
+            {
+                await _repository.CreateAsync(ProductItem);
+            }
+        }        
+    }
+}
---
+++ services/FastBuy.Stocks/src/FastBuy.Stocks.Services/Consumers/ProductItemDeleteConsumer.cs
@@ -0,0 +1,28 @@
+﻿using FastBuy.Products.Contracts.Events;
+using FastBuy.Shared.Library.Repository.Abstractions;
+using FastBuy.Stocks.Entities;
+using MassTransit;
+using System.Linq.Expressions;
+
+namespace FastBuy.Stocks.Services.Consumers
+{
+    class ProductItemDeleteConsumer : IConsumer<ProductDeletedEvent>
+    {
+        private readonly IRepository<ProductItem> _repository;
+
+        public ProductItemDeleteConsumer(IRepository<ProductItem> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Consume(ConsumeContext<ProductDeletedEvent> context)
+        {
+            var message = context.Message;
+
+            Expression<Func<ProductItem, bool>> filter = x => x.Id == message.Id;
+
+            if (!await _repository.ExistAsync(filter)) return;
+
+            await _repository.DeleteAsync(message.Id);
+        }
+    }
+}
