e25f92ccb628b845c17a4a79d10c612779dbccdc
arielRas <ariel.ras.90@gmail.com>
2025-04-19 11:15:59 -0300

    Se crea la solucion FastBuy.Orders, en el se crea el proyecto FastBuy.Orders.Api. Se instalan las librerias Swashbuckle.AspNetCore, MassTransit.AspNetCore, MassTransit.RabbitMQ. Se configura Swagger.

---
+++ services/FastBuy.Orders/src/FastBuy.Orders.Api/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace FastBuy.Orders.Api.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            //Controller registration
+            services.AddControllers();
+
+            //configure Swagger as an endpoint explorer
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
---
+++ services/FastBuy.Orders/src/FastBuy.Orders.Api/Program.cs
@@ -0,0 +1,33 @@
+
+using FastBuy.Orders.Api.Extensions;
+
+namespace FastBuy.Orders.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddApplicationServices(builder.Configuration)
+                            .AddPresentation();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
