a10d032cf024b3f9d7c69d7c57808b1470c9dced
arielRas <ariel.ras.90@gmail.com>
2025-04-23 20:56:01 -0300

    En FatBuy.Orders se crea la clase OrderService

--- services/FastBuy.Orders/src/FastBuy.Orders.Services/Implementations/OrderService.cs
+++ services/FastBuy.Orders/src/FastBuy.Orders.Services/Implementations/OrderService.cs
@@ -1,26 +1,39 @@
 ﻿using FastBuy.Orders.Contracts.DTOs;
+using FastBuy.Orders.Contracts.Events;
 using FastBuy.Orders.Repository.Repositories.Abstractions;
 using FastBuy.Orders.Services.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FastBuy.Orders.Services.Mappers;
+using MassTransit;
 
 namespace FastBuy.Orders.Services.Implementations
 {
     class OrderService : IOrderService
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IPublishEndpoint _publisher;
 
-        public OrderService(IOrderRepository orderRepository)
+        public OrderService(IOrderRepository orderRepository, IPublishEndpoint publisher)
         {
             _orderRepository = orderRepository;
+            _publisher = publisher;
         }
 
-        public Task CreateAsync(OrderRequestDto orderDto)
+        public async Task CreateAsync(OrderRequestDto orderDto)
         {
-            throw new NotImplementedException();
+            var newId = Guid.NewGuid();
+
+            var newOrder = orderDto.ToEntity(newId);
+
+            await _orderRepository.CreateAsync(newOrder);
+
+            var orderCreateEvent = new OrderCreatedEvent 
+            {
+                CorrelationId = newId,
+                OrderItems = orderDto.OrderItems,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            await _publisher.Publish(orderCreateEvent);
         }
     }
 }
---
+++ services/FastBuy.Orders/src/FastBuy.Orders.Services/Mappers/OrderMapper.cs
@@ -0,0 +1,31 @@
+﻿using FastBuy.Orders.Contracts.DTOs;
+using FastBuy.Orders.Entities;
+
+namespace FastBuy.Orders.Services.Mappers
+{
+    internal static class OrderMapper
+    {
+        public static Order ToEntity(this OrderRequestDto dto, Guid orderId)
+        {
+            return new Order
+            {
+                Id = orderId,
+                Items = dto.OrderItems.Select(oi => oi.ToEntity(orderId)).ToList(),
+                CreatedAt = DateTimeOffset.UtcNow,
+                LastUpdate = DateTimeOffset.UtcNow,
+                State = "Acepted"
+            };
+        }
+
+
+        public static OrderItem ToEntity(this OrderItemRequestDto dto, Guid orderId)
+        {
+            return new OrderItem
+            {
+                OrderId = orderId,
+                ProductId = dto.ProductId,
+                Quantity = dto.Quantity
+            };
+        }
+    }
+}
